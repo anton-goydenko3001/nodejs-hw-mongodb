75354db000f3f1226990f2d6e0d538f69c380a43
Anton <goydenko.30@gmail.com>
2025-01-13 14:36:26 +0200

    fix router post/contacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -51,7 +51,17 @@ export const getContactByIdController = async (req, res, next) => {
 
 export const createContactController = async (req, res) => {
   const { _id: userId } = req.user;
-  const contact = await createContact({ ...req.body, userId });
+  const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    }
+  }
+
+  const contact = await createContact({ ...req.body, photo: photoUrl, userId });
 
   res.status(201).json({
     status: 201,
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(3).max(20),
+  name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().min(3).max(30),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
